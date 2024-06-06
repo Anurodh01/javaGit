822e5e61552203bf0a15617fb619dea92e7d98c7
anurodhsingh <anurodh.singh@nagarro.com>
2024-06-06 21:15:07 +0530

    Added Data.java file

---
+++ Data.java
@@ -0,0 +1,48 @@
+public class Data {
+
+    private String name;
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
