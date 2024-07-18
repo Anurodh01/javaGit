0a7de18db55a147c98cd0255ec98b45e7e231af1
anurodhsingh <anurodh.singh@nagarro.com>
2024-07-18 20:24:31 +0530

    Study on Date And Calendar Class

---
+++ date/CalendarClass.java
@@ -0,0 +1,33 @@
+package date;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+
+/**
+ * Calendar Class to UnderStand the Utility of this class over Date Class (java.util)
+ */
+public class CalendarClass {
+    public static void main(String[] args) {
+        Calendar currentDate = Calendar.getInstance();
+        int date = currentDate.get(Calendar.DATE);
+        int month = currentDate.get(Calendar.MONTH)+1;
+        int year = currentDate.get(Calendar.YEAR);
+        Calendar birthDate = Calendar.getInstance();
+        birthDate.set(1999,Calendar.NOVEMBER,17);
+        calculateAge(birthDate, currentDate);
+    }
+
+    /**
+     * Calculating the years old with help of ChronoUnit
+     * @param startDate
+     * @param endDate
+     */
+    public static void calculateAge(Calendar startDate, Calendar endDate){
+        LocalDate start = startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate end = endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        long years = ChronoUnit.YEARS.between(start, end);
+        System.out.printf("I am %d years old",years);
+    }
+}
---
+++ date/DateClass.java
@@ -0,0 +1,42 @@
+package date;
+import java.util.Date;
+
+/**
+ * date class(java.util package) ---> Deprecated
+ */
+public class DateClass {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        Date birthDate = new Date();
+        birthDate.setDate(17);
+        birthDate.setMonth(9-1);
+        birthDate.setYear(1999-1900);
+        calculateAge(birthDate, currentDate);
+    }
+
+    /**
+     * Calculating the age on basis of birthdate
+     * @param startDate
+     * @param endDate
+     */
+    public static void calculateAge(Date startDate, Date endDate){
+        long timeDifference = endDate.getTime() - startDate.getTime();
+
+        //total years
+        long totalMilliSecondsInAYear = 365 * 24 * 60 * 60 * 1000L;
+        long year = timeDifference/totalMilliSecondsInAYear;
+        long remainingTimeDifference = timeDifference % totalMilliSecondsInAYear;
+
+        //total month
+        long totalMilliSecondsInAMonth = 30 * 24 * 60 * 60 * 1000L;
+        long month = remainingTimeDifference / totalMilliSecondsInAMonth;
+        remainingTimeDifference %= totalMilliSecondsInAMonth;
+
+        //total days
+        long totalMilliSecondsInADay = 24 * 60 * 60 * 1000L;
+        long day = remainingTimeDifference / totalMilliSecondsInADay;
+        remainingTimeDifference %= totalMilliSecondsInADay;
+
+        System.out.println("I am "+ year+ " years "+ month +" month "+day+" Day old." );
+    }
+}
