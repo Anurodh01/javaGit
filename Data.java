de15496bba4ace32ee747c4618cee9418b2f0628
anurodhsingh <anurodh.singh@nagarro.com>
2024-07-18 20:03:39 +0530

    Revert "Deleted Random files"
    This reverts commit 6a717a27d0b819a1adf8e615eb5064c4770cc580.

---
+++ Data.java
@@ -0,0 +1,48 @@
+public class Data{
+    private Integer id;
+    private String email;
+    private String name;
+    private Integer age;
+
+    public Data() {
+    }
+
+    public Data(int id, String email, String name, Integer age) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
