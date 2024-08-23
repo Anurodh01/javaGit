210c0d198d40023eee587a087427bb4e17a5ed10
Anurodh Singh <67429000+Anurodh01@users.noreply.github.com>
2024-08-23 19:37:42 +0530

    Update Base.java

--- generics/Base.java
+++ generics/Base.java
@@ -9,7 +9,7 @@ class ADerievedBase extends ABase{
     public ADerievedBase(){
         System.out.println("Derived Constructor called");
     }
-
+    
     @Override
     public void test() {
         System.out.println("TEST method implemented");
@@ -18,5 +18,6 @@ class ADerievedBase extends ABase{
 public class Base {
     public static void main(String[] args) {
         ADerievedBase derievedBase = new ADerievedBase();
+        derievedBase.test();
     }
 }
