59368dbcfae03eda475ee76b4d2ce74ef7425e74
anurodhsingh <anurodh.singh@nagarro.com>
2024-08-24 07:58:02 +0530

    feauture/branch1 updated CalculateAge.java file

--- date/CalculateAge.java
+++ date/CalculateAge.java
@@ -4,7 +4,7 @@ import java.time.*;
 import java.time.temporal.ChronoUnit;
 
 public class CalculateAge {
-    public Calculate(){
+    public CalculateAge(){
         System.out.println("origin/master updated");
     }
     public static void main(String[] args) {
